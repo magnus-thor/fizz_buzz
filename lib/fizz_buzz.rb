1670264805cd6f91596bb8d6d13e1908493c1b92
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-07 14:09:28 +0200

    fizz_buzz now handles normal behaviour

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,11 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
-  if number % 3 == 0
+  if number % 15 == 0
+    'fizz buzz'
+  elsif number % 5 == 0
+    'buzz'
+  elsif number % 3 == 0
     'fizz'
   else
     number
