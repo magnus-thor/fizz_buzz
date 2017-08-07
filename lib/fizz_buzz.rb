7ce0366cc51b8088ddd85509baa564ebe3e3a6d0
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-07 15:03:49 +0200

    added a sad path to the program

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,24 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
-  if number % 15 == 0
+  if (number.is_a? Integer) && number > 0
+    if number % 15 == 0
     'fizz buzz'
-  elsif number % 5 == 0
+    elsif number % 5 == 0
     'buzz'
-  elsif number % 3 == 0
+    elsif number % 3 == 0
     'fizz'
-  else
+    else
     number
+    end
+  else
+    case number
+    when Float
+      'it is a float number'
+    when String
+      'it is a string'
+    else
+      'it is a negative number'
+    end
   end
 end
