8b6b5f2d005a6a0ca24923f61a7dbdd8679abfb2
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-07 14:17:13 +0200

    added tests for float, negative number and string

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -19,4 +19,17 @@ end
 it "returns 'fizz buzz' if number is divisible with 15" do
   expect(fizz_buzz(15)).to eq 'fizz buzz'
 end
+
+it "returns 'it is a string' if it is a string" do
+  expect(fizz_buzz("some string")).to eq 'it is a string'
+end
+
+it "returns 'its a negative number' if it is a negative number" do
+  expect(fizz_buzz(-1)).to eq 'it is a negative number'
+end
+
+it "returns 'its a float number' if it is a float" do
+  expect(fizz_buzz(4.12)).to eq 'its a float number'
+end
+
 end
